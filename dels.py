bf20e1a50600d81ddee4934cf696a56ac9fc3748
salter <s7262370@gmail.com>
2024-11-05 22:43:20 +0800

    change settings

--- dels.py
+++ dels.py
@@ -1,2 +1,10 @@
+import sys,os
+import pandas as pd
+project_dir = "/projectname/"
+sys.path.append(project_dir)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
+import django
+django.setup()
+
 from members.models import Tour, Site, Company, Region
 Company.objects.filter(id=5).delete()
