2b871464f4fff495865f91a63e9a8478534a1992
salter <s7262370@gmail.com>
2024-11-16 12:18:35 +0800

    fix mysql

--- dump_data.py
+++ dump_data.py
@@ -9,6 +9,6 @@ import json
 from django.core.management import call_command
 
 # Open the output file in write mode with UTF-8 encoding
-with open('fixture_batch_0.json', 'w', encoding='utf-8') as f:
+with open('data.json', 'w', encoding='utf-8') as f:
     # Call dumpdata and write to the file
     call_command('dumpdata', stdout=f)
